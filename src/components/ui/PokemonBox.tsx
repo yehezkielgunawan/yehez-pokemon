ecd72cba5f22181e1f707c592a641a082c5b3cc7
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-11-28 19:51:53 +0700

    feat(main page): :sparkles: add base load pokemon data without pagination

---
+++ src/components/ui/PokemonBox.tsx
@@ -0,0 +1,47 @@
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Image } from "@chakra-ui/image";
+import { Box, Stack, Text } from "@chakra-ui/layout";
+import React from "react";
+
+import { getPokemonID } from "@/functions/helpers/getPokemonId";
+
+type PokemonBoxProps = {
+  name: string;
+  url: string;
+};
+
+const PokemonBox = ({ name, url }: PokemonBoxProps) => {
+  const { colorMode } = useColorMode();
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      bg="orange.300"
+      borderRadius={10}
+      p={[4, 8]}
+      boxShadow={colorMode === "light" ? "dark-lg" : "outline"}
+    >
+      <Stack spacing={3}>
+        <Text
+          fontSize={["md", "xl"]}
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip="text"
+        >
+          <b>{name}</b>
+        </Text>
+        <Text fontSize="lg" color="gray">
+          <b>#{getPokemonID(url)}</b>
+        </Text>
+      </Stack>
+      <Image
+        alt="pokemon-image"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonID(
+          url
+        )}.png`}
+      />
+    </Box>
+  );
+};
+
+export default PokemonBox;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,57 @@
-import { Heading } from "@chakra-ui/react";
-import React from "react";
+import { Heading, SimpleGrid, Image, Center } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
+import MotionBox from "@/components/motion/MotionBox";
 import OwnedPokemonBox from "@/components/ui/OwnedPokemonBox";
+import PokemonBox from "@/components/ui/PokemonBox";
 import Main from "@/components/wrapper/Main";
+import { getPokemonList } from "@/functions/services/fetcher";
+import { PokemonList } from "@/functions/services/types";
 
 const Index = () => {
+  const [pokemonList, setPokemonList] = useState<PokemonList>();
+
+  const getPokemons = async (limit?: number, offset?: number) => {
+    return await getPokemonList(limit, offset).then((res: PokemonList) =>
+      setPokemonList(res)
+    );
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
   return (
     <Main>
       <OwnedPokemonBox />
       <Heading as="h2">Pokedex</Heading>
+      {pokemonList ? (
+        <SimpleGrid columns={[1, 2]} spacing={3}>
+          {pokemonList.results.map((pokemon, index) => {
+            return (
+              <PokemonBox key={index} name={pokemon.name} url={pokemon.url} />
+            );
+          })}
+        </SimpleGrid>
+      ) : (
+        <Center>
+          <MotionBox
+            animate={{ y: 40 }}
+            transition={{
+              repeat: Infinity,
+              duration: 0.5,
+              repeatType: "reverse",
+            }}
+          >
+            <Image
+              alt="Pokeball"
+              src="https://image.flaticon.com/icons/png/512/1068/1068780.png"
+              objectFit="contain"
+              w={36}
+            />
+          </MotionBox>
+        </Center>
+      )}
     </Main>
   );
 };
