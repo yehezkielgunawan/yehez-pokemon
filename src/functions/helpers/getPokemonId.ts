a883a98bb28466bc44e7b67416835b433f1cf450
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-11-29 15:35:54 +0700

    docs(functions folder): :bulb: give quick explanation comment

--- src/functions/helpers/getPokemonId.ts
+++ src/functions/helpers/getPokemonId.ts
@@ -1,3 +1,5 @@
+// get Pokemon ID based on pattern at front_default key in sprites object
+//Example :  front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
 export const getPokemonID = (url: string) => {
   const splittedURL = url.split("/");
   return splittedURL[splittedURL.length - 2];
--- src/functions/services/fetcher.ts
+++ src/functions/services/fetcher.ts
@@ -4,6 +4,7 @@ import { DetailPokemonResType, PokemonList } from "./types";
 
 import { baseURL } from "@/constants/base";
 
+// All the fetcher function is here
 export const getPokemonList = async (offset: number = 0) => {
   return await axios
     .get(`${baseURL}/pokemon?offset=${offset}&limit=20`)
